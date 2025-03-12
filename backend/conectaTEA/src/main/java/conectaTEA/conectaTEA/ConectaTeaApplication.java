a8924d167c77b9894d60d3d26c288775299d6496
LuizFelixxx <luizfelix.dev@gmail.com>
2025-03-12 16:17:03 -0300

    :bug:  fix: define main class for backend application

---
+++ backend/conectaTEA/src/main/java/conectaTEA/conectaTEA/ConectaTeaApplication.java
@@ -0,0 +1,13 @@
+package conectaTEA.conectaTEA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaTeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaTeaApplication.class, args);
+	}
+
+}
---
+++ backend/conectaTEA/src/test/java/conectaTEA/conectaTEA/ConectaTeaApplicationTests.java
@@ -0,0 +1,13 @@
+package conectaTEA.conectaTEA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaTeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
