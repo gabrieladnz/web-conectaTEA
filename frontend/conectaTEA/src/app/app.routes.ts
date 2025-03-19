93ba33ade493f6412da498d0818191d6ceba8f07
Gabriela Diniz <gadisa10@outlook.com>
2025-03-18 23:26:23 -0300

    ✨ feat: definição das rotas iniciais no Front-End

--- frontend/conectaTEA/src/app/app.routes.ts
+++ frontend/conectaTEA/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './modules/register/register.component';
+import { LoginComponent } from './modules/login/login.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { ChatComponent } from './modules/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/auth/login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'auth',
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent,
+            },
+            {
+                path: 'register',
+                component: RegisterComponent,
+            },
+        ],
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+    },
+    {
+        path: 'chat',
+        component: ChatComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '/auth/login',
+    },
+];
