30ba8d45dd7883f36946d13adfd4490ba2921a4c
Gabriela Diniz <gadisa10@outlook.com>
2025-03-26 22:36:56 -0300

    ✨ feat: criação da sidebar com o menu de navegação, logout e ajuda

--- frontend/conectaTEA/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/conectaTEA/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,36 @@
+// Libs
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterModule } from '@angular/router';
+
+// Services
+import { AuthService } from '../../../core/services/auth/auth.service';
 
 @Component({
-  selector: 'app-sidebar',
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+    selector: 'app-sidebar',
+    imports: [MatIconModule, RouterModule],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+    constructor(
+        private router: Router,
+        private authentication: AuthService,
+    ) {}
 
+    /**
+     * @description Realiza o logout do usuário
+     */
+    protected async logout(): Promise<void> {
+        /** TODO: Validar método com integração */
+        await this.authentication.logout();
+        this.router.navigate(['/login']);
+    }
+
+    /**
+     * @description Abre a modal de informações
+     */
+    protected openHelp(): void {
+        /** TODO: Implementar modal com informações sobre o site */
+    }
 }
