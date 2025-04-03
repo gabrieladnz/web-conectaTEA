d42555aa57a9b7284c011500a05ca7d248b35f6f
Gabriela Diniz <gadisa10@outlook.com>
2025-04-03 09:52:20 -0300

    🎨 feat: definindo estrutura da dashboard

--- frontend/conectaTEA/src/app/modules/dashboard/dashboard.component.ts
+++ frontend/conectaTEA/src/app/modules/dashboard/dashboard.component.ts
@@ -1,11 +1,16 @@
+// Libs
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+// Components
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    selector: 'app-dashboard',
+    imports: [SidebarComponent, RouterModule],
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-
+    constructor(private router: Router) {}
 }
