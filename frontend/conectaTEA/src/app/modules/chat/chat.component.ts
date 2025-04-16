de9b42440bc0b02d596d5582a32d27737cc081d5
Gabriela Diniz <gabriela.diniz@newrizon.global>
2025-04-15 22:34:52 -0300

    🎨 feat: estruturação do componente de chat com dados mocados temporariamente

--- frontend/conectaTEA/src/app/modules/chat/chat.component.ts
+++ frontend/conectaTEA/src/app/modules/chat/chat.component.ts
@@ -1,12 +1,14 @@
 // Libs
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 // Components
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 
 @Component({
     selector: 'app-chat',
-    imports: [SidebarComponent],
+    imports: [SidebarComponent, MatButtonModule, MatIconModule],
     templateUrl: './chat.component.html',
     styleUrl: './chat.component.scss',
 })
