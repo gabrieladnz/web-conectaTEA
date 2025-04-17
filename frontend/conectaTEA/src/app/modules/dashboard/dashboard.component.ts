7ded42c7371b3cbc52ceeb543cd4d61d1a2396b6
Gabriela Diniz <gadisa10@outlook.com>
2025-04-17 16:48:40 -0300

    ✨ feat: refatoração da dashboard, aplicando um novo estilo com dados mockados

--- frontend/conectaTEA/src/app/modules/dashboard/dashboard.component.ts
+++ frontend/conectaTEA/src/app/modules/dashboard/dashboard.component.ts
@@ -2,19 +2,14 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 // Components
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
-import { CalendarComponent } from '../../shared/components/calendar/calendar.component';
 
 @Component({
     selector: 'app-dashboard',
-    imports: [
-        SidebarComponent,
-        RouterModule,
-        MatButtonModule,
-        CalendarComponent,
-    ],
+    imports: [SidebarComponent, RouterModule, MatButtonModule, MatIconModule],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss',
 })
