32d7d9616ba72f74e08f0e441efa732b52883a73
Elias Junior <santanaeliasjunior.@gmail.com>
2025-04-22 20:10:06 -0300

    🐛 fix: Correção de erro ao enviar mensagem, fazer login e logout.

--- backend/conectaTEA/src/main/java/conectaTEA/conectaTEA/controllers/ChatController.java
+++ backend/conectaTEA/src/main/java/conectaTEA/conectaTEA/controllers/ChatController.java
@@ -2,9 +2,11 @@ package conectaTEA.conectaTEA.controllers;
 
 import conectaTEA.conectaTEA.dtos.socket.ChatMessageDTO;
 import conectaTEA.conectaTEA.services.rest.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -12,14 +14,18 @@ public class ChatController {
 
     private MessageService messageService;
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     public ChatController(MessageService messageService){
         this.messageService = messageService;
     }
 
     @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/room.{roomId}")
-    public ChatMessageDTO sendMessage(@DestinationVariable String roomId, ChatMessageDTO message){
+    public ChatMessageDTO sendMessage(ChatMessageDTO message){
+        String roomId = message.roomId().toString();
         messageService.save(message);
+        messagingTemplate.convertAndSend("/topic/room." + roomId, message);
         return message;
     }
 }
